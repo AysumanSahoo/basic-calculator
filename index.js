1e61d2071cda83414785614de102105640d61f6d
AysumanSahoo <sahooaysuman56@gmail.com>
2025-10-21 20:38:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+function appendValue(value) {
+    display.value += value;
+}
+function clearDisplay() {
+    display.value = '';
+}
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
+function toggleSign() {
+    if (display.value) {
+
+        if (display.value.startsWith('-')) {
+            display.value = display.value.slice(1);
+        } else {
+
+            display.value = '-' + display.value;
+        }
+    }
+}
+
